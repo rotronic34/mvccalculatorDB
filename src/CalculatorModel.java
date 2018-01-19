5968132545fc4d0910ab29d13493196f5b6794b1
Roeut Chak <roeut.chak@gmail.com>
2018-01-19 00:26:14 -0500

    created model for MVC calculator

--- src/CalculatorModel.java
+++ src/CalculatorModel.java
@@ -1,3 +1,13 @@
 public class CalculatorModel {
 
+    public int calculationValue;
+
+    public void addTwoNumbers(int num1, int num2){
+        calculationValue = num1 + num2;
+    }
+
+    public int getCalculationValue() {
+        return calculationValue;
+    }
+
 }
